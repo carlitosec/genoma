7612a9c0da74ec53fe35f4311d2cd1a85efcc23d
Carlos Carrillo <carlossmonk@hotmail.com>
2021-03-14 00:09:00 -0500

    Version componentes updated.

--- src/main/java/co/torre/genoma/controller/UserController.java
+++ src/main/java/co/torre/genoma/controller/UserController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
 
 import co.torre.genoma.model.Experience;
@@ -18,27 +18,29 @@ import co.torre.genoma.service.UserService;
  * @author ccarrillo
  *
  */
-@SessionScoped
+// @SessionScoped
+@RequestScoped
 @ManagedBean
 public class UserController {
-	
+
 	@Inject
 	private UserService userService;
-	
+
 	private String userName;
 	private User user;
-	
+
 	public String findUserInfo() {
 		user = userService.getUserInfo(getUserName());
 		return "userInfo";
 	}
-	
+
 	public List<Experience> getExperiences() {
 		return user.getExperiences().stream().filter(e -> e.getCategory().equals("jobs")).collect(Collectors.toList());
 	}
-	
+
 	public List<Experience> getStudies() {
-		return user.getExperiences().stream().filter(e -> e.getCategory().equals("education")).collect(Collectors.toList());
+		return user.getExperiences().stream().filter(e -> e.getCategory().equals("education"))
+				.collect(Collectors.toList());
 	}
 
 	/**
@@ -49,7 +51,8 @@ public class UserController {
 	}
 
 	/**
-	 * @param userName the userName to set
+	 * @param userName
+	 *            the userName to set
 	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
@@ -63,12 +66,11 @@ public class UserController {
 	}
 
 	/**
-	 * @param user the user to set
+	 * @param user
+	 *            the user to set
 	 */
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	
 
 }
